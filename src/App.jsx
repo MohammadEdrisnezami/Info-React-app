6042ddc7a1fa54317a12d3795aba21c1b8924c31
Mohammad Edris <abdulkarimnezami@gmail.com>
2025-02-08 21:07:24 +0430

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const  Info=<div>
+    <h3>Name: Mohammad Edris</h3>
+    <h3>LastName: Nezamy</h3>
+    <h3>Emial: mohammadedrisnezamy@gmail.com</h3>
+    
+  </div>
+  return (
+    <>
+         {Info}
+    </>
+  )
+}
+
+export default App
